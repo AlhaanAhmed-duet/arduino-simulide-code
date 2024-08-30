4a24e21d2f6e4a7d19ad21fbe52da09a9aa3c23e
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2024-08-30 10:03:27 +0500

    Updated corrected code

--- ledchaser/ledchaser.ino
+++ ledchaser/ledchaser.ino
@@ -1,18 +1,33 @@
-int ledOne = 1;
-int ledTwo = 2;
-int ledThree = 3;
-int ledFour = 4;
-int ledFive = 5;
-
-
-void setup () {
-	pinMode(ledOne, OUTPUT);
-	pinMode(ledTwo, OUTPUT);
-	pinMode(ledThree, OUTPUT);	
-	pinMode(ledFour, OUTPUT);
-	pinMode(ledFive, OUTPUT);
+const int pins[5] = {2, 3, 4, 5, 6};  
+void setup() {
+	// Intialize pins using for-loop
+  for (int i = 0; i <= sizeof(pins) / sizeof(pins[0]); i++) {  // Corrected length calculation
+    pinMode(pins[i], OUTPUT);
+  }
+  Serial.begin(9600);
 }
 
-void loop () {
-
+void loop() {
+  forwardMove();
+  Serial.println("Forward");
+  delay(100);
+  backwardMove();
+  Serial.println("Backward");
+  delay(100);
+}
+// Lights will move forward
+void forwardMove() {
+  for (int i = 0; i <= sizeof(pins) / sizeof(pins[0]); i++) {  // Corrected length calculation
+    digitalWrite(pins[i], HIGH);
+    delay(60);
+	digitalWrite(pins[i], LOW);
+  }
+}
+// Lights will move backward
+void backwardMove() {
+  for (int i = (sizeof(pins) / sizeof(pins[0])) - 1; i >= 0; i--) {  // Corrected bounds and decrement order
+    digitalWrite(pins[i], HIGH);
+    delay(60);
+	digitalWrite(pins[i], LOW);
+  }
 }
